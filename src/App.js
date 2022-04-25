fc651567be507cad331bd4dc620a81f78ea5490e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-25 19:42:23 +0530

    add an img tag inside the nav tag

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <nav>
-          
+          <img src='https://kfinancial.com/wp-content/uploads/2019/02/amazon-logo-vector-png-vector-png-free-amazon-logos-705.jpg' alt='Amazon Logo' />
         </nav>
       </header>
     </div>
