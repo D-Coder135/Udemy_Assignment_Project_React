1101ef2c0d2e600fe9efd983bb58019e1092aba0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-27 13:58:19 +0530

    add a div tag

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
       <div className={style.MainContainer}>
         <div className={style.ProductPreview}>
           <img src='https://imgur.com/iOeUBV7.png' alt='Product Preview'/>
+
+          <div></div>
         </div>
 
         <div className={style.ProductData}>
