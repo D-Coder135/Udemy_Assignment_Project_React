e7757b63c0fa25f7bcd8abedbd167d75456f3c55
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-27 10:31:18 +0530

    add a img tag inside the div

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
           <h3 className={style.SectionHeading}>Select Color</h3>
           <div>
-            
+            <img />
           </div>
         </div>
       </div>
