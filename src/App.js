171d697fc5a43c26eb5225063380146d1858ebb8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-27 10:20:53 +0530

    add the ProductData.description inside the p tag as text

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
         <div className={style.ProductData}>
           <h1 className={style.ProductTitle}>{ProductData.title}</h1>
-          <p></p>
+          <p>{ProductData.description}</p>
         </div>
       </div>
     </div>
