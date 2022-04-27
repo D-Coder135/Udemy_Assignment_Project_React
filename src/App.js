58ec4ddca3c1b8fdc39a09fb6bbb41ef73f28d06
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-27 10:38:51 +0530

    update the src attribute for the img tags

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ function App() {
 
           <h3 className={style.SectionHeading}>Select Color</h3>
           <div>
+            <img className={style.ProductImage} src="https://imgur.com/iOeUBV7.png" alt='Red Colored Watch'/>
             <img className={style.ProductImage} src="https://imgur.com/PTgQlim.png" alt='Red Colored Watch'/>
-            <img className={style.ProductImage} src="https://imgur.com/PTgQlim.png" alt='Red Colored Watch'/>
-            <img className={style.ProductImage} src="https://imgur.com/PTgQlim.png" alt='Red Colored Watch'/>
-            <img className={style.ProductImage} src="https://imgur.com/PTgQlim.png" alt='Red Colored Watch'/>
+            <img className={style.ProductImage} src="https://imgur.com/Mplj1YR.png" alt='Red Colored Watch'/>
+            <img className={style.ProductImage} src="https://imgur.com/xSIK4M8.png" alt='Red Colored Watch'/>
           </div>
         </div>
       </div>
