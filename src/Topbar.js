265e094c45dd44fac679d593ae005b691ec0c84d
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-05-11 18:11:31 +0530

    add some code inside the return statement of the functional component

--- src/Topbar.js
+++ src/Topbar.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Topbar = () => {
-
+    return(
+        <nav className={style.Topbar}>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjbEGoAAOT2e9grED4G3hqjiM1afks8nvii7PZGDe0dJeKnmrONwswHeD_N1gbDGCHNi8&usqp=CAU' alt='Amazon Logo' />
+        </nav>
+    );
 }
 
 export default Topbar;
