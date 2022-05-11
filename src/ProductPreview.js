b828fb673142fada936bd1d4b4779760e92d4171
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-05-11 13:20:47 +0530

    comment out some code and add some part of the code

--- src/ProductPreview.js
+++ src/ProductPreview.js
@@ -3,17 +3,18 @@ import React from "react";
 import style from './ProductPreview.module.css';
 
 const ProductPreview = (props) => {
+    const currentMinutes = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
     return(
         <div className={style.ProductPreview}>
             <img src='https://imgur.com/iOeUBV7.png' alt='Product Preview'/>
 
-            {/* <div className={style.TimeSection}>
+            <div className={style.TimeSection}>
             <p>{`${new Date().getHours()}:${currentMinutes}`}</p>
-            </div> */}
-            <div className={style.HeartBeatSection}>
+            </div>
+            {/* <div className={style.HeartBeatSection}>
             <i class="fas fa-heartbeat"></i>
             <p>78</p>
-            </div>
+            </div> */}
         </div>
     );
 }
