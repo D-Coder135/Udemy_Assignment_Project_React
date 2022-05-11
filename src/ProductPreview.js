43cdca944afac1aec6e995f08558553bcbd13cc6
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-05-11 21:26:45 +0530

    pass props value inside the src attribute inside the img tag

--- src/ProductPreview.js
+++ src/ProductPreview.js
@@ -6,7 +6,7 @@ const ProductPreview = (props) => {
     const currentMinutes = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
     return(
         <div className={style.ProductPreview}>
-            <img src='https://imgur.com/iOeUBV7.png' alt='Product Preview'/>
+            <img src={props.currentPreviewImage} alt='Product Preview'/>
 
             <div className={style.TimeSection}>
             <p>{`${new Date().getHours()}:${currentMinutes}`}</p>
