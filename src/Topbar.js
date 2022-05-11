bd77b261d20a4a052b3f5f88034e6e86e3f7723e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-05-11 18:13:42 +0530

    add a header tag inside the return statement

--- src/Topbar.js
+++ src/Topbar.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const Topbar = () => {
     return(
+        <header className="App-header">
         <nav className={style.Topbar}>
           <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjbEGoAAOT2e9grED4G3hqjiM1afks8nvii7PZGDe0dJeKnmrONwswHeD_N1gbDGCHNi8&usqp=CAU' alt='Amazon Logo' />
         </nav>
+        </header>
     );
 }
 
