11b5556b95170b1cec5dcb58cd61182e0c8543e5
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-05-11 18:09:25 +0530

    cut out some part of the code

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <nav className={style.Topbar}>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjbEGoAAOT2e9grED4G3hqjiM1afks8nvii7PZGDe0dJeKnmrONwswHeD_N1gbDGCHNi8&usqp=CAU' alt='Amazon Logo' />
-        </nav>
+        
       </header>
 
       <div className={style.MainContainer}>
